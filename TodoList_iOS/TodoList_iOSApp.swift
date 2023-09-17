f0d6021a268948c3c59ec4877ec6132ba2cc0248
Eduardo Reis <eduardoreisdev@gmail.com>
2023-09-17 15:27:16 -0300

    Initial Commit

---
+++ TodoList_iOS/TodoList_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_iOSApp.swift
+//  TodoList_iOS
+//
+//  Created by Eduardo Reis on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
